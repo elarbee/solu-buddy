c37154521d5eb8dac5fb8f80c30ed3c2810bec85
wh01683 <wh01683@georgiasouthern.edu>
2016-02-25 13:38:39 -0500

    Can't update values in "answer" section of solution.html.
    Tried using .val(answer), .innerHTML = answer, .text = answer, .textContent = answer, etc..

--- single-solution/SolutionPage.js
+++ single-solution/SolutionPage.js
@@ -37,11 +37,28 @@ function fill_fields(){
     var total_volume = $("#totalVolume").val();
     var target_solution_concentration = $("#solution_concentration").val();
 
-    var mass_of_solute_to_add =
-        SingleSolution(target_solution_concentration, total_volume,
-            solute_molecular_weight).solid();
+    var single_solution = SingleSolution(target_solution_concentration, total_volume,
+        solute_molecular_weight);
 
-    var target_solution_concentration = $("#solution_concentration").val();
+    var mass_of_solute_to_add = single_solution.solid();
+
+    var molarity = solute_compound.molecular_weight()/total_volume;
+
     $("#massToAdd").val(mass_of_solute_to_add + "g");
 
+    $("#molarity_span1").innerHTML = molarity;
+    $("#molarity_span2").innerHTML = molarity;
+
+    $("#solvent_span1").val(solvent_compound.formula());
+    $("#solvent_span2").val(solvent_compound.formula());
+    $("#solvent_span3").val(solvent_compound.formula());
+
+    $("#solute_span1").val(solute_compound.formula());
+    $("#solute_span2").val(solute_compound.formula());
+    $("#solute_span3").val(solute_compound.formula());
+
+    $("#volume_span1").val(total_volume);
+    $("#volume_span2").val(total_volume);
+
+    $("#mass_span").val(mass_of_solute_to_add);
 };
