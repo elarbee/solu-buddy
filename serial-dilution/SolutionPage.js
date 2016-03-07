fd5ae93a0e3b02a64a87014cad80101ab95822a9
elarbee <aelarbee@gmail.com>
2016-03-07 15:35:27 -0500

    Serial Dilution Functionality
    Added in about 50% of the serial dilution functionality.

--- serial-dilution/SolutionPage.js
+++ serial-dilution/SolutionPage.js
@@ -1,13 +1,48 @@
 $(function(){
     
-    //Answer page control
-    $("#nextButton").click(function(){
-        $("#answerDiv").slideDown("slow");
-    });
-    
     //Back arrow for answer page
     $("#arrowContainer img").click(function(){
        $("#answerDiv").slideUp("fast");
     });
+    
+    
+    //This if statement will check for valid input, right now it only evaluates as true.
+    var validInput = true;
+    if(true){
+        
+        //Next Button Click Handler
+        $("#nextButton").click(function(){
+            
+            //Show answer div
+            $("#answerDiv").slideDown("slow");
+            
+            //Used in loop to hold dilution flask div
+            var dilutionFlask;
+            
+            //Get number of dilution flasks to prepare
+            var numFlasks = $("#numDilutions").val();
+            
+            //Iterate through number of flasks inputted and add them to the page.
+            for(i = 0;i<numFlasks-1;i++){
+                dilutionFlask = $("#dilutionFlask").clone();
+                //Set flask number
+                dilutionFlask.find("#flaskNum").text(i+2);
+                $("#dilutionFlasksDiv").append(dilutionFlask);
+            }
+            
+            //Set the value of all of the blue arrow divs to the value specified
+            $(".blueArrow p").text($("#volumeTransferred").val()+" ML");
+            
+            //Remove arrow from last flask
+            $("#dilutionFlasksDiv").children().last().find(".blueArrow").remove();
+            
+            
+            
+        });
+        
+
+        
+    }
+    
     
 });
