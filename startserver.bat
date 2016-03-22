8e44c33b6649ecb845cf9ca8049c7c709d68825c
wh01683 <wh01683@georgiasouthern.edu>
2016-03-22 17:40:47 -0400

    Why are these modified.

--- startserver.bat
+++ startserver.bat
@@ -1,3 +1,3 @@
 REM This is a simple batch file that will start a HTTP server in the directory it's executed in
 REM as long as python is install and properly configured in the windows %PATH%
-python -m SimpleHTTPServer
+python -m http.server 8000
