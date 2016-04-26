6c6caf974a3f5d10d6c511934a358aa629ea6f7b
positronik <matthewstreker@gmail.com>
2016-04-25 22:39:13 -0400

    Login and Signup Modals updated

--- index.php
+++ index.php
@@ -7,7 +7,8 @@
     <link rel="stylesheet" type="text/css" href="/static/css/header-styles.css">
     <link rel="stylesheet" type="text/css" href="/static/css/style.css">
     <link rel="stylesheet" type="text/css" href="/static/css/navBar.css">
-
+	
+	
     <!-- jQuery -->
     <script src="/static/js/jquery-1.11.3.min.js"></script>
     <script src="/static/js/bootstrap.js"></script>
--- loginModal.php
+++ loginModal.php
@@ -1,3 +1,5 @@
+<link rel="stylesheet" type="text/css" href="/shared-content/AccountModalStyle.css">
+
 <!-- Modal 1-->
 <div id="modal-1" class="modal fade" style="margin-top:9em;" role="dialog">
     <div class="modal-dialog modal-sm">
@@ -11,7 +13,7 @@
                 <form action="/accounts/login.php?next=<?php echo $_SERVER['REQUEST_URI']; ?>" method="post">
                     <input type = "text" name="username" id="textbox" placeholder="Username"><br>
                     <input type = "password" name="password"  id="textbox" placeholder="Password"><br>
-                    <div class="modal-footer footer">
+                    <div class="modal-footer">
                         <input type="submit" class="submit" name="submit" value="Log In">
                         <input type="button" class="cancel" name="signup" data-dismiss="modal" value="Cancel">
                     </div>
